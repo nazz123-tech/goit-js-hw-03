f1e81cb64ec70d11e82883784a7c591573383b7a
nazar <road2thedream1@gmail.com>
2025-09-21 01:51:44 +0300

    hw03

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,14 @@
-function getElementWidth(content,padding,border){
-    return (Number.parseFloat(content))+2*(Number.parseFloat(padding))+2*(Number.parseFloat(border));
+function filterArray(numbers,value){
+    const numbersArray = [];
+    for(const number of numbers){
+        if(number>value){
+            numbersArray.push(number);
+        }
+    }
+    return numbersArray;
 }
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
